3d08e88affc7395692584f4de8b21c2689d024ab
HolyCrossGleam7 <gianneschamar@gmail.com>
2026-03-07 13:02:09 +0200

    Add new network components to __all__ export

--- network/__init__.py
+++ network/__init__.py
@@ -1,4 +1,17 @@
 from .bluetooth_manager import BluetoothManager
 from .network_sync import NetworkSync
+from .transaction_broadcaster import TransactionBroadcaster
+from .wifi_discovery import WiFiDiscovery
+from .wifi_server import WiFiServer
+from .wifi_client import WiFiClient
+from .network_manager import NetworkManager
 
-__all__ = ['BluetoothManager', 'NetworkSync']
+__all__ = [
+    'BluetoothManager',
+    'NetworkSync',
+    'TransactionBroadcaster',
+    'WiFiDiscovery',
+    'WiFiServer',
+    'WiFiClient',
+    'NetworkManager'
+]
